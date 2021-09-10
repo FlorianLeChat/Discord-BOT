4164f1b0f0e2051c0a4d64fc97e69137c88def05
FlorianLeChat <floriantrayon942@gmail.com>
2021-09-10 21:52:31 +0200

    Remise en place du gestionnaire des activités personnalisées

--- modules/activity_manager.js
+++ modules/activity_manager.js
@@ -15,36 +15,17 @@ const activities = [
 	"One Two Three, viva la guerre d'Algérie"
 ];
 
-const updateActivity = async() => {
-	let index = Math.floor(Math.random() * (activities.length - 1) + 1);
+const activity = () =>
+{
+	var index = Math.floor(Math.random() * (activities.length - 1) + 1);
 
 	bot.user.setActivity(activities[index], {type: "WATCHING"})
-		.then(() => console.log(`Nouvelle activité Discord : \`${activities[index]}\`.`))
-		.catch(console.error);
+		.then(() =>
+			console.log(`Nouvelle activité Discord : \`${activities[index]}\`.`)
+		)
+		.catch(error =>
+			console.log(`[Error] Impossible de définir le message d'activity, ${error}.`)
+		);
 };
 
-bot.on("ready", async() => {
-	console.log(`Le robot \"${bot.user.username}\" a démarré avec succès.`);
-	console.log(`Le robot est actuellement connecté sur ${bot.guilds.cache.size} serveurs.`);
-
-	updateActivity()
-
-	// Note : 1000 ms => 1 seconde * <temps en secondes> (ex: 600 secondes = 10 minutes).
-	setInterval(() => {
-		updateActivity()
-	}, 1000 * 600);
-
-	// Seulement pour le système du NNN.
-	// createMessage();
-
-	// setInterval(() => {
-	// 	let timestamp = new Date();
-	// 	timestamp = timestamp.getHours() + ":" + timestamp.getMinutes() + ":" + timestamp.getSeconds();
-
-	// 	if (timestamp == "0:0:0")
-	// 	{
-	// 		days++;
-	// 		createMessage();
-	// 	}
-	// }, 1000);
-});
+module.exports = activity
