18e3d5e993125a419a66d17c961f2515c20f51ba
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-09-10 22:01:55 +0200

    Correction des activités à cause de la mise à jour v14

--- modules/activity_manager.js
+++ modules/activity_manager.js
@@ -15,17 +15,13 @@ const activities = [
 	"One Two Three, viva la guerre d'Algérie"
 ];
 
-const activity = () =>
+const activity = (bot) =>
 {
 	var index = Math.floor(Math.random() * (activities.length - 1) + 1);
 
 	bot.user.setActivity(activities[index], {type: "WATCHING"})
-		.then(() =>
-			console.log(`Nouvelle activité Discord : \`${activities[index]}\`.`)
-		)
-		.catch(error =>
-			console.log(`[Error] Impossible de définir le message d'activity, ${error}.`)
-		);
+
+	console.log(`Nouvelle activité Discord : \`${activities[index]}\`.`)
 };
 
 module.exports = activity
