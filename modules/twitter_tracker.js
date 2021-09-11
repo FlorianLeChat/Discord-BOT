e00dbcceabaf149c6adc94eb6b5bfbace0939e8c
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-09-11 17:08:29 +0200

    Les actualités/erreurs Twitter sont désormais envoyées dynamiquement

--- modules/twitter_tracker.js
+++ modules/twitter_tracker.js
@@ -1,91 +1,146 @@
 //
-// Suivi de certains comptes Twitter.
+// Suivi de certaines actualités Twitter.
 // Source : https://www.npmjs.com/package/twitter-api-v2
 //
-const {bearerToken, twitterChannel} = require("../config.json");
-const {ETwitterStreamEvent, TwitterApi} = require("twitter-api-v2");
+const { twitterToken, red, orange } = require("../data/__internal__.json")
+const { ETwitterStreamEvent, TwitterApi } = require("twitter-api-v2")
 
 module.exports.streamTwitter = async(bot) => {
 
+	// On récupère tous les canaux pour envoyer les messages d'actualités.
+	// Note : ce service est désactivé si aucun salon est trouvé.
+	var identifiers = []
+
+	bot.channels.cache.forEach(channel => {
+
+		if (channel.name.includes("news") || channel.name.includes("actualités"))
+		{
+			identifiers.push(channel.id)
+		}
+
+	})
+
+	if (identifiers.length <= 0)
+		return
+
 	// On initialise le module de l'API Twitter.
-	const client = new TwitterApi(bearerToken);
+	const client = new TwitterApi(twitterToken)
 
 	// On récupère les règles de filtrage actuelles avant de les supprimer.
-	const rules = await client.v2.streamRules();
+	const rules = await client.v2.streamRules()
 
-	if (rules.data?.length) {
+	if (rules.data?.length)
+	{
 		await client.v2.updateStreamRules({
 			delete: {
 				ids: rules.data.map(rule => rule.id)
 			}
-		});
+		})
 	}
 
 	// On ajoute les comptes à suivre pour l'affichage des nouveaux tweets.
 	await client.v2.updateStreamRules({
 		add: [
-			{ value: "from:133663801"},
 			{ value: "from:217749896" }, 			// Marine <3
 			{ value: "from:338985020" }, 			// AFP (FR)
 			{ value: "from:24744541" }, 			// Le Monde (FR)
 			{ value: "from:1001029225476972545" }, 	// Ministère de l'intérieur (FR)
 			{ value: "from:2097571" }, 				// CNN International (US)
-			{ value: "from:742143" }, 				// BBC World (UK)
-		],
-	});
+			{ value: "from:742143" } 				// BBC World (UK)
+		]
+	})
 
 	// On lance la recherche des tweets en indiquant les données que l'API doit nous retourner.
 	const stream = await client.v2.searchStream({
-		"tweet.fields": [
-			"author_id"
-		]
-	});
+		"tweet.fields": ["author_id"]
+	})
 
-	stream.autoReconnect = true;
-	stream.on(ETwitterStreamEvent.Data, async(eventInfo) =>
-	{
+	stream.autoReconnect = true
+
+	stream.on(ETwitterStreamEvent.Data, async(eventInfo) => {
+
 		// On vérifie que le tweet n'est pas un RT.
 		if (eventInfo.data.text.startsWith("RT @"))
-			return;
+			return
 
 		// On vérifie alors la nationalité du compte.
-		var countryFlag = ":flag_fr:";
+		var countryFlag = ":flag_fr:"
 
 		switch (eventInfo.data.author_id)
 		{
 			case "2097571": // CNN
-				countryFlag = ":flag_us:";
-				break;
+				countryFlag = ":flag_us:"
+				break
 			case "742143": // BBC
-				countryFlag = ":flag_gb:";
-				break;
+				countryFlag = ":flag_gb:"
+				break
 			case "1001029225476972545": // Ministère de l'Intérieur
-				countryFlag = ":rotating_light: @everyone";
-				break;
+				countryFlag = ":rotating_light: @everyone"
+				break
 			case "217749896": // Marine <3
-				countryFlag += " <@183272411167326209> <@407212462740340747>";
-				break;
-		};
+				countryFlag += " <@183272411167326209> <@407212462740340747>"
+				break
+		}
 
-		// On envoie ensuite le message dans le canal prévu à cet effet.
+		// On envoie ensuite le message dans les canaux récupérés précédemment.
 		var userInfo = await client.v2.user(eventInfo.data.author_id)
 
-		bot.channels.fetch(twitterChannel)
-			.then(channel =>
+		identifiers.forEach(identifier => {
+
+			bot.channels.fetch(identifier).then(channel => {
+
 				channel.send(`https://twitter.com/${userInfo.data.username}/status/${eventInfo.data.id} ${countryFlag}`)
-			)
-			.catch(error =>
-				console.log(`[Error] Impossible de trouver le salon pour répliquer les messages Twitter, ${error}.`)
-			);
-	});
 
-	//
+			})
+
+		})
+
+	})
+
+	// On affiche une notification dans tous les canaux d'actualités si une erreur de connexion se produit.
+	var username = bot.user.username
+	var avatar = bot.user.avatarURL()
+
 	stream.on(ETwitterStreamEvent.ConnectionError, error =>
-		console.log("Connection error!", error),
-	);
 
+		identifiers.forEach(identifier => {
+
+			bot.channels.fetch(identifier).then(channel => {
+
+				const messageEmbed = new discord.MessageEmbed()
+					.setColor(orange)
+					.setAuthor(username, avatar)
+					.setTitle("Erreur API")
+					.setDescription("Une erreur de connexion s'est produite avec les serveurs Twitter.")
+					.addField("Message d'erreur:", error);
+
+				channel.send({ embeds: [ messageEmbed ] });
+
+			})
+
+		})
+
+	)
+
+	// On affiche une notification dans tous les canaux d'actualités si la connexion se perd.
 	stream.on(ETwitterStreamEvent.ConnectionClosed, () =>
-		console.log("Connection has been closed."),
-	);
 
-};
+		identifiers.forEach(identifier => {
+
+			bot.channels.fetch(identifier).then(channel => {
+
+				const messageEmbed = new discord.MessageEmbed()
+					.setColor(red)
+					.setAuthor(username, avatar)
+					.setTitle("Erreur API")
+					.setDescription("La connexion entre le robot et les serveurs Twitter a été interrompue.")
+
+				channel.send({ embeds: [ messageEmbed ] });
+
+			})
+
+		})
+
+	)
+
+}
