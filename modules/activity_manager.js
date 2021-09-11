befcebfc2cc55a72ee55bd5f7646730543dfc0f0
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-09-11 13:55:47 +0200

    Modifications mineures dans les activités personnalisées

--- modules/activity_manager.js
+++ modules/activity_manager.js
@@ -1,5 +1,5 @@
 //
-// Gestion du démarrage
+// Activités personnalisées du bot.
 //
 const activities = [
 	"Marine contre les incivilités",
@@ -13,13 +13,28 @@ const activities = [
 	"Made by Florian :D",
 	"Wallah j'te braque",
 	"One Two Three, viva la guerre d'Algérie"
-];
+]
 
-module.exports.updateActivity = (bot) =>
-{
-	var index = Math.floor(Math.random() * (activities.length - 1) + 1);
+module.exports = {
 
-	bot.user.setActivity(activities[index], {type: "WATCHING"})
+	// Définition de l'activité.
+	setActivity: (bot, name, type = "WATCHING") => {
 
-	console.log(`Nouvelle activité Discord : \`${activities[index]}\`.`)
-};
+		bot.user.setActivity(name, {
+			type: type
+		})
+
+		console.log(`Nouvelle activité Discord : « ${name} ».`)
+
+	},
+
+	// Randomisation de l'activité.
+	randomActivity: (bot) => {
+
+		var index = Math.floor(Math.random() * (activities.length - 1) + 1)
+
+		module.exports.setActivity(bot, activities[index])
+
+	}
+
+}
