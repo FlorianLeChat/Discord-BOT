367ed931dcaa2c45ed1defd6f52c112847dacbf9
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-09-11 18:18:50 +0200

    Ajout d'une notification Discord au démarrage

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// Initialisation du module.
-const discord = require("discord.js")
-
+//
 // Initialisation du bot.
+//
+const discord = require("discord.js")
 const bot = new discord.Client({
 
 	// GUILS + GUID_MEMBERS + GUILD_BANS + GUILD_MESSAGES + DIRECT_MESSAGES
+	// https://discord.com/developers/docs/topics/gateway#list-of-intents
 	intents: new discord.Intents(4615),
 
 	// https://discordjs.guide/additional-info/changes-in-v13.html#allowed-mentions
@@ -18,8 +19,10 @@ const bot = new discord.Client({
 
 })
 
+//
 // Lancement du gestionnaire des activités personnalisées.
 // Note : 1000 ms => 1 seconde * <temps en secondes> (exemple : 600 secondes = 10 minutes).
+//
 const delay = 1000 * 600
 const { randomActivity } = require("./modules/activity_manager.js")
 
@@ -35,18 +38,37 @@ bot.on("ready", () => {
 
 })
 
+//
 // Lancement du suivi des comptes Twitter.
-const {streamTwitter} = require("./modules/twitter_tracker.js")
-streamTwitter(bot)
-
-// Finalisation de l'initialisation.
-const {discordToken} = require("./data/__internal__.json")
-
-bot.login(discordToken)
+//
+const { streamTwitter } = require("./modules/twitter_tracker.js")
 
 bot.on("ready", () => {
 
-	console.log(`Le robot « ${bot.user.username} » a démarré avec succès.`)
-	console.log(`Le robot est actuellement connecté sur ${bot.guilds.cache.size} serveurs.`)
+	streamTwitter(bot)
+
+})
+
+//
+// Finalisation de l'initialisation.
+//
+const settings = require("./data/__internal__.json")
+
+bot.login(settings.discordToken)
+
+bot.on("ready", () => {
+
+	bot.channels.fetch(settings.masterChannel).then(channel => {
+
+		const messageEmbed = new discord.MessageEmbed()
+			.setColor(settings.greenColor)
+			.setAuthor(bot.user.username, bot.user.avatarURL())
+			.setTitle("Démarrage terminé")
+			.setDescription(`Le robot « ${bot.user.username} » a démarré avec succès.`)
+			.addField("Serveurs présents :", bot.guilds.cache.size.toString());
+
+		channel.send({ embeds: [ messageEmbed ] });
+
+	})
 
 })
