b955e5ed584d0cc3698015c4d5eb6ce47de8ec84
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-11-02 21:08:29 +0100

    Correction du système de sauvegarde/chargement des données du NNN via MySQL

--- modules/no_nut_november.js
+++ modules/no_nut_november.js
@@ -2,61 +2,86 @@
 // Événement du "No Nut November".
 //
 const excluders = [];
-const { setSaveData } = require( "./sql_database.js" );
+const { setSaveData, getSaveData } = require( "./sql_database.js" );
 
-let days = new Date().getDate() - 1;
-let users = [];
+let days = 0;
 
 module.exports =
 {
 	// Création du message d'informations des jours passés.
-	createMessage: ( bot ) =>
+	createMessage: async ( bot ) =>
 	{
-		// On recherche tous les canaux de l'événement avant d'afficher ou d'éditer le message.
-		// Note : on coupe l'exécution du script si aucun salon n'est trouvé.
-		let cache = bot.channels.cache;
-		let channels = [];
+		// On recherche tous les serveurs et salons accueillant l'événement.
+		// Note : on coupe l'exécution du script si aucun serveur/salon n'est trouvé.
+		const cache = bot.channels.cache;
 
-		days++;
+		let guilds = [];
+		let channels = [];
 
 		for ( const channel of cache.values() )
 		{
+			const identifier = channel.guild.id;
+
 			if ( channel.name.includes( "nnn" ) || channel.name.includes( "no-nut-november" ) )
 			{
+				if ( !guilds.includes( identifier ) )
+					guilds.push( identifier ); // On ajoute l'identifiant du serveur seulement une seule fois.
+
 				channels.push( channel.id );
 			}
 		}
 
-		if ( channels.length <= 0 )
+		days = new Date().getDate(); // Mise à jour du nombre de jours.
+
+		if ( guilds.length <= 0 || channels.length <= 0 )
 			return;
 
-		// On créé le message d'information.
+		// On créé par la suite le message d'information grâce aux informations récupérées précédemment.
+		// Note : une solution plus simple serait de faire cette étape dans la précédente boucle, sans succès...
+		let users = [];
 		let phrase = `:spy: :sweat_drops: Statut « **No Nut November** » (Jour ${ days }) :\n`;
 		phrase += "\n:point_right: Pour prendre en compte votre présence, réagissez à ce message !\n";
 
-		for ( let identifier in users )
+		for ( const identifier of guilds.values() )
 		{
-			const user = bot.users.cache.get( identifier );
+			const guild = bot.guilds.cache.get( identifier );
 
-			// On exclut tous les robots.
-			if ( user.bot )
-				return;
+			guild.members.fetch().then( async ( members ) =>
+			{
+				// On itére à travers tous les membres de chaque serveur.
+				for ( const member of members.values() )
+				{
+					// On exclut tous les robots.
+					if ( member.user.bot )
+						continue;
 
-			// On exclut les utilisateurs qui ne peuvent pas participer.
-			if ( excluders.includes( user.id ) )
-				return;
+					// On exclut les utilisateurs qui ne peuvent pas participer.
+					let identifier = member.user.id;
 
-			phrase += `- __${ user.username }__ : J+${ users[ user.id ] || 0 }\n`;
+					if ( excluders.includes( identifier ) )
+						continue;
+
+					// Dans le cas contraire, on récupère les informations de l'utilisateur concernant l'événement.
+					await getSaveData( bot, identifier, "NNN", 0 ).then( result =>
+					{
+						if ( result > 0 && !users.includes( identifier ) ) // Pas besoin d'afficher les utilisateurs qui ne participent pas.
+						{
+							phrase += `- __${ member.user.username }__ : J+${ result }\n`;
+							users.push( identifier );
+						}
+					} );
+				};
+			} );
 		}
 
-		phrase += "\n**Ces valeurs sont actualisées tous les jours à minuit.** :point_up_2:";
+		phrase += "\n:rotating_light: **Les valeurs ci-dessous sont actualisées tous les jours à minuit.** :rotating_light:\n\n";
 
-		// On itére à travers tous les salons en mémoire.
+		// On itére après à travers tous les salons en mémoire.
 		for ( const identifier of channels.values() )
 		{
 			bot.channels.fetch( identifier ).then( channel =>
 			{
-				// On récupère un certain nombre de messages provenant du canal.
+				// On récupère ensuite un certain nombre de messages provenant du canal.
 				// Note : on limite la recherche des messages à un nombre de 10 pour éviter d'aller trop loin.
 				let edited = false;
 
@@ -78,7 +103,7 @@ module.exports =
 						}
 					}
 
-					// Dans le cas contraire, on envoie ensuite un nouveau message.
+					// Dans le cas contraire, on envoie un nouveau message.
 					if ( !edited )
 					{
 						channel.send( phrase )
@@ -105,19 +130,12 @@ module.exports =
 		if ( excluders.includes( user.id ) )
 			return;
 
-		// On vérifie alors si on doit incrémenter une journée ou non à l'utilisateur.
-		users[ user.id ] = days;
+		// On enregistre enfin le nombre de jours passés dans la base de données.
+		var count = days;
 
-		if ( type == true )
-		{
-			// On évite le spam des réactions pour briser la logique de l'événement.
-			if ( users[ user.id ] >= days + 1 )
-				return;
+		if ( type === true )
+			count++;
 
-			users[ user.id ]++;
-		}
-
-		// On sauvegarde enfin cette information dans la base de données.
-		setSaveData( bot, user.id, "NNN", users[ user.id ] );
+		setSaveData( bot, user.id, "NNN", count );
 	}
 };
