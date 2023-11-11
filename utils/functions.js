c08787adf9ef4a98523fa913cba66cdcb0bb6aa7
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-11-11 13:53:45 +0100

    Removed old utilities functions and legacy modules

--- utils/functions.js
+++
@@ -1,7 +0,0 @@
-//
-// Permet d'imposer un temps de pause avant de continuer l'exécution du code.
-//
-module.exports.sleep = ( milliseconds ) =>
-{
-	return new Promise( resolve => setTimeout( resolve, milliseconds ) );
-};
