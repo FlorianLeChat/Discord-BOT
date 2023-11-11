3b3657e818853f485c7962de5c8c8030cdc72cde
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-11-11 18:44:01 +0100

    Improved overall type support for DiscordJS API slash commands

--- @types/discord.d.ts
+++ @types/discord.d.ts
@@ -1,8 +1,36 @@
-// Types d'extensions des propriétés du client DiscordJS.
-import { type Collection } from "discord.js";
+// Typage de certains objets de DiscordJS (commandes, temps d'attente, etc.).
+//  Source : https://github.com/MericcaN41/discordjs-v14-template-ts/blob/main/src/types.d.ts
+import type {
+	Message,
+	CacheType,
+	Collection,
+	SlashCommandBuilder,
+	PermissionResolvable,
+	ModalSubmitInteraction,
+	AutocompleteInteraction,
+	ChatInputCommandInteraction
+} from "discord.js";
+
+export interface Command {
+	name: string;
+	aliases: Array<string>;
+	execute: ( message: Message, args: Array<string> ) => void;
+	cooldown?: number;
+	permissions: Array<PermissionResolvable>;
+}
+
+export interface SlashCommand {
+	modal?: ( interaction: ModalSubmitInteraction<CacheType> ) => void;
+	command: SlashCommandBuilder;
+	execute: ( interaction: ChatInputCommandInteraction ) => void;
+	cooldown?: number;
+	autocomplete?: ( interaction: AutocompleteInteraction ) => void;
+}
 
 declare module "discord.js" {
 	interface Client {
-		commands: Collection<string, unknown>;
+		commands: Collection<string, Command>;
+		cooldowns: Collection<string, number>;
+		slashCommands: Collection<string, SlashCommand>;
 	}
 }
